80b7672c7560c03f665e87ef4fbf5d6d641ce46b
김하챤 <dbtldk12@naver.com>
2025-06-14 18:54:31 +0900

    Fix : Crouch
    웅크린 상태에서 움직이지 못하게 하기.
    스크립트는 수정했으나 아직도 움직임

--- Assets/_Sia/NewFox(key)/PlayerController.cs
+++ Assets/_Sia/NewFox(key)/PlayerController.cs
@@ -19,9 +19,9 @@ public class PlayerController : MonoBehaviour
     public GameObject Hp1;
 
     bool IsGround;
+    bool isCrouching = false;
     SpriteRenderer spriteRenderer;
 
-    //바닥 점프시 통과
     int playerLayer, platformLayer;
 
     public AudioSource mySfx;
@@ -38,40 +38,41 @@ public class PlayerController : MonoBehaviour
 
         playerLayer = LayerMask.NameToLayer("Player");
         platformLayer = LayerMask.NameToLayer("Platform");
-
-   
     }
 
-    
     void Update()
     {
-        if (Mathf.Abs(rigid.velocity.x) < 0.2f)
+        // 웅크리기 상태 체크
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            anim.SetBool("IsWalking", false);
-          
+            anim.SetTrigger("Crouch");
+            isCrouching = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.X))
+        {
+            isCrouching = false;
+        }
+
+        // 걷기 애니메이션 (단, 웅크릴 땐 꺼짐)
+        if (!isCrouching && Mathf.Abs(rigid.velocity.x) > 0.2f)
+        {
+            anim.SetBool("IsWalking", true);
         }
         else
         {
-            anim.SetBool("IsWalking", true);
-            
-            
+            anim.SetBool("IsWalking", false);
         }
 
-
-
-
-
+        // 점프 체크
         IsGround = Physics2D.OverlapCircle(pos.position, checkRadius, islayer);
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsGround == true)
-        { 
+        if (Input.GetKeyDown(KeyCode.Space) && IsGround && !isCrouching)
+        {
             rigid.velocity = Vector2.up * Jump;
             anim.SetBool("IsJumping", true);
             JumpSound();
-            
         }
 
-        //바닥 점프시 통과
+        // 바닥 점프 시 통과
         if (rigid.velocity.y > 0)
         {
             Physics2D.IgnoreLayerCollision(playerLayer, platformLayer, true);
@@ -82,56 +83,48 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        float hor = Input.GetAxis("Horizontal");
-        rigid.velocity = new Vector2(hor * 4, rigid.velocity.y);
-
-        
-        if (hor > 0)
+        if (!isCrouching)
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
+            float hor = Input.GetAxis("Horizontal");
+            rigid.velocity = new Vector2(hor * 4, rigid.velocity.y);
+
+            if (hor > 0)
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            else if (hor < 0)
+                transform.eulerAngles = new Vector3(0, 180, 0);
         }
-        else if (hor < 0)
+        else
         {
-            transform.eulerAngles = new Vector3(0, 180, 0);
+            // 웅크리면 이동 차단
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
         }
 
-        if (rigid.velocity.y < 0) { 
+        // 점프 상태 해제
+        if (rigid.velocity.y < 0)
+        {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
-            if(rayHit.collider != null)
+            if (rayHit.collider != null && rayHit.distance < 1f)
             {
-                if (rayHit.distance < 1f)
-                    anim.SetBool("IsJumping", false);
+                anim.SetBool("IsJumping", false);
             }
         }
-        if (Input.GetKey(KeyCode.X)) // 웅크리기
-        {
-            anim.SetTrigger("Crouch");
-
-        }
     }
-    //public int Hp_ = 3;
 
-    void OnCollisionEnter2D(Collision2D collision) // 플레이어 피격 이벤트
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Boss"))
         {
             HitSound();
             OnDamaged(collision.transform.position);
         }
-        if (collision.gameObject.CompareTag("Boss"))
-        {
-            HitSound();
-            OnDamaged(collision.transform.position);
-        }
-
     }
+
     void OnDamaged(Vector2 targetPos)
     {
-        
         gameObject.layer = 12;
         spriteRenderer.color = new Color(0.6981132f, 0.1522784f, 0.1522784f, 0.4f);
         int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
@@ -140,18 +133,16 @@ public class PlayerController : MonoBehaviour
 
         Invoke("OffDamaged", 2f);
         Invoke("Super", 5f);
-
     }
 
-    void OffDamaged() // 피격 후 2초 후에 레이어 원래대로
+    void OffDamaged()
     {
-        
         spriteRenderer.color = new Color(0.6981132f, 0.6981132f, 0.6981132f, 1);
     }
-    void Super() // 피격 후 5초동안 무적
+
+    void Super()
     {
         gameObject.layer = 11;
-        
     }
 
     void JumpSound()
