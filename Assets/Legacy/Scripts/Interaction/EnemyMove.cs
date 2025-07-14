7e393547751929a131d94bee0cf85a4141824fbf
김하챤 <dbtldk12@naver.com>
2025-07-14 18:07:21 +0900

    Fix : EnemyMove
    - 주석제거

--- Assets/Legacy/Scripts/Interaction/EnemyMove.cs
+++ Assets/Legacy/Scripts/Interaction/EnemyMove.cs
@@ -24,7 +24,6 @@ public class EnemyMove : MonoBehaviour
     {
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
-        //발판 유뮤 체크, 없으면 멈춤
         Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y);
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 2, LayerMask.GetMask("Platform"));
@@ -33,18 +32,15 @@ public class EnemyMove : MonoBehaviour
             Turn();
     }
 
-    void Think()    //몬스터 움직임 AI함수
+    void Think()
     {
-        // 움직이고 난 후에 다음에 어떻게 해야하지?
         nextMove = Random.Range(-1, 2);
 
         float nextThinkTime = Random.Range(2f, 5f);
-        Invoke("Think", nextThinkTime); // 재귀함수
+        Invoke("Think", nextThinkTime);
 
-        //몬스터 애니메이션
         anim.SetInteger("WalkSpeed", nextMove);
 
-        //몬스터 방향전환
         if(nextMove !=0 )
             spriteRenderer.flipX = nextMove == 1;
     }
